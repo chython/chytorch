ca4651d43861d5bc49fda0cbece27152c4c50579
Ramil Nugmanov <rnugmano@its.jnj.com>
2022-10-24 21:40:12 +0200

    Implemented adopted TransformerDecoderLayer.

--- chytorch/nn/transformer.py
+++ chytorch/nn/transformer.py
@@ -56,4 +56,40 @@ class EncoderLayer(Module):
         return None, a
 
 
-__all__ = ['EncoderLayer']
+class DecoderLayer(Module):
+    r"""DecoderLayer based on torch.nn.TransformerDecoderLayer, but batch always first and returns also attention.
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model (required).
+        dropout: the dropout value (default=0.1).
+        activation: the activation function of the intermediate layer. Default: GELU.
+        layer_norm_eps: the eps value in layer normalization components (default=1e-5).
+    """
+    def __init__(self, d_model, nhead, dim_feedforward, dropout=0.1, activation=GELU, layer_norm_eps=1e-5):
+        super().__init__()
+        self.mha_attn = MultiheadAttention(d_model, nhead, dropout=dropout)
+
+        self.linear1 = Linear(d_model, dim_feedforward)
+        self.linear2 = Linear(dim_feedforward, d_model)
+        self.norm1 = LayerNorm(d_model, eps=layer_norm_eps)
+        self.norm2 = LayerNorm(d_model, eps=layer_norm_eps)
+        self.dropout1 = Dropout(dropout)
+        self.dropout2 = Dropout(dropout)
+        self.dropout3 = Dropout(dropout)
+        self.activation = activation()
+
+    def forward(self, x: Tensor, mem: Tensor, attn_mask: Tensor, *,
+                need_embedding: bool = True, need_weights: bool = False) -> Tuple[Optional[Tensor], Optional[Tensor]]:
+        tx = x.transpose(1, 0)  # switch Batch and Sequence. torch-1.8 compatible.
+        tm = mem.transpose(1, 0)
+        e, a = self.mha_attn(tx, tm, tm, attn_mask=attn_mask, need_weights=need_weights)
+        if need_embedding:
+            e.transpose_(1, 0)  # switch Sequence and Batch
+            e = self.norm1(x + self.dropout1(e))
+            return self.norm2(e + self.dropout3(self.linear2(self.dropout2(self.activation(self.linear1(e)))))), a
+        return None, a
+
+
+__all__ = ['EncoderLayer', 'DecoderLayer']
