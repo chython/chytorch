69c72c6ba3db75de77757c03bf01cf050b78fc65
khakimova <aigul03.14@gmail.com>
2022-11-02 15:48:39 +0300

    UPD: class for data and training

--- examples/reference_start.py
+++ examples/reference_start.py
@@ -1,16 +1,15 @@
 import pandas as pd
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 from chython import smiles
+from torch.optim import Adam
 from torch.utils.data import DataLoader
+from typing import Optional
 
-from chytorch.nn import MoleculeEncoder
-from chytorch.utils import data
-from chytorch.utils.data import chained_collate
+from chytorch.nn import MoleculeEncoder, Slicer
+from chytorch.utils.data import MoleculeDataset, chained_collate, collate_molecules
 
-BATCH_SIZE = 64
-LEARNING_RATE = 0.01
-EPOCHS = 3
 torch.manual_seed(1)
 
 # check GPU
@@ -18,123 +17,149 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
 
-class Data:
-    def __init__(self, path_to_csv):
-        self.train_containers = None
-        self.y_train = None
-        self.test_containers = None
-        self.y_test = None
-        self.path_to_csv = path_to_csv
+class PandasData(pl.LightningDataModule):
+    def __init__(
+        self,
+        csv: str,
+        structure: str,
+        property: str,
+        dataset_type: str,
+        batch_size: int = 32,
+    ):
+        super().__init__()
+        self.train_x = None
+        self.train_y = None
+        self.test_x = None
+        self.test_y = None
+        self.validation_x = None
+        self.validation_y = None
+        self.csv = csv
+        self.structure = structure
+        self.property = property
+        self.dataset_type = dataset_type
+        self.batch_size = batch_size
 
-    def read_csv(self):
-        print("Read csv file")
-        df = pd.read_csv(self.path_to_csv)
-        df = df[["std_smiles", "activity", "dataset"]]
-        mol_containers_train = [
-            smiles(i) for i in df[df.dataset == "train"].std_smiles[:10000]
-        ]
-        mol_containers_test = [
-            smiles(i) for i in df[df.dataset == "test"].std_smiles[:100]
-        ]
-        y_train = df[df.dataset == "train"].activity[:10000]
-        y_test = df[df.dataset == "test"].activity[:100]
-        self.train_containers = mol_containers_train
-        self.y_train = torch.Tensor(y_train.to_list())
-        self.test_containers = mol_containers_test
-        self.y_test = torch.Tensor(y_test.to_list())
+    def prepare_data(self):
+        df = pd.read_csv(self.csv)
+        df = df[[self.structure, self.property, self.dataset_type]]
+        return df
 
-    def get_dataloader(self, data_type):
-        print("Prepare dataloader")
-        if data_type == "train":
-            X_train = data.MoleculeDataset(self.train_containers, add_cls=True)
-            dataset_loader = DataLoader(
-                dataset=torch.utils.data.TensorDataset(X_train, self.y_train),
-                collate_fn=chained_collate(data.collate_molecules, torch.stack),
-                batch_size=BATCH_SIZE,
-                shuffle=True,
-            )
-            return dataset_loader
+    def setup(self, stage: Optional[str] = None):
+        df = self.prepare_data()
+        if stage == "fit" or stage is None:
+            df_train = df[df.dataset == "train"][:100]
+            mols = [smiles(m) for m in df_train[self.structure]]
+            self.train_x = MoleculeDataset(mols)
+            self.train_y = torch.Tensor(df_train[self.property].to_numpy())
 
-        elif data_type == "test":
-            X_test = data.MoleculeDataset(self.test_containers, add_cls=True)
-            dataset_loader = DataLoader(
-                dataset=torch.utils.data.TensorDataset(X_test, self.y_test),
-                collate_fn=chained_collate(data.collate_molecules, torch.stack),
-                batch_size=BATCH_SIZE,
-            )
-            return dataset_loader
+        if stage == "validation" or stage is None:
+            df_validation = df[df.dataset == "validation"][:100]
+            mols = [smiles(m) for m in df_validation[self.structure]]
+            self.validation_x = MoleculeDataset(mols)
+            self.validation_y = torch.Tensor(df_validation[self.property].to_numpy())
+
+        if stage == "test" or stage is None:
+            df_test = df[df.dataset == "test"][:100]
+            mols = [smiles(m) for m in df_test[self.structure]]
+            self.test_x = MoleculeDataset(mols)
+            self.test_y = torch.Tensor(df_test[self.property].to_numpy())
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=torch.utils.data.TensorDataset(self.train_x, self.train_y),
+            collate_fn=chained_collate(collate_molecules, torch.stack),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=torch.utils.data.TensorDataset(self.train_x, self.train_y),
+            collate_fn=chained_collate(collate_molecules, torch.stack),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=torch.utils.data.TensorDataset(self.test_x, self.test_y),
+            collate_fn=chained_collate(collate_molecules, torch.stack),
+            batch_size=self.batch_size,
+        )
 
 
-class Network(torch.nn.Module):
-    def __init__(self):
+class Modeler:
+    def __init__(self, loss_function, epochs: int, learning_rate, csv: str):
+        self.network = None
+        self.optimizer = None
+        self.loss_function = loss_function
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.csv = csv
+
+    def train_loop(self, dataset_loader):
+        size = len(dataset_loader.dataset)
+        for batch, (X, y) in enumerate(dataset_loader, 1):
+            # compute prediction and loss
+            predictions = self.network(X)
+            loss = self.loss_function(predictions.squeeze(-1), y)
+            # backpropagation
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            if batch % 10 == 0:
+                loss_v, current = loss.item(), batch * len(X[0])
+                print(f"loss: {loss_v:>3f} [{current:>5d}/{size:>5d}]")
+
+    def validation_loop(self, dataset_loader):
+        size = len(dataset_loader.dataset)
+        num_batches = len(dataset_loader)
+        self.network.eval()
+        test_loss, correct = 0, 0
+        with torch.no_grad():
+            for X, y in dataset_loader:
+                predictions = self.network(X)
+                test_loss += self.loss_function(predictions.squeeze(-1), y).item()
+                correct += (predictions.argmax(1) == y).type(torch.float).sum().item()
+        test_loss = test_loss / num_batches
+        correct = correct / size
+        print(
+            f"Validation Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+        )
+
+    def fit(self):
         """
-        Define model
+        Run model training
         """
-        super(Network, self).__init__()
-        self.input_encoder = MoleculeEncoder()
-        self.linear = nn.Sequential(
+        dataset = PandasData(
+            csv=self.csv,
+            structure="std_smiles",
+            property="activity",
+            dataset_type="dataset",
+            batch_size=10,
+        )
+        dataset.setup()
+        self.network = nn.Sequential(
+            MoleculeEncoder(),
+            Slicer(slice(None), 0),
             nn.Linear(in_features=1024, out_features=512),
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(in_features=512, out_features=1),
             nn.Sigmoid(),
         )
-
-    def forward(self, X):
-        E = self.input_encoder(X)
-        X = self.linear(E[:, 0])
-        return X
-
-
-def train_loop(network, dataset_loader, loss_func, optimizer):
-    size = len(dataset_loader.dataset)
-    for batch, (X, y) in enumerate(dataset_loader, 1):
-        # compute prediction and loss
-        predictions = network(X)
-        loss = loss_func(predictions.squeeze(-1), y)
-        # backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if batch % 10 == 0:
-            loss_v, current = loss.item(), batch * len(X[0])
-            print(f"loss: {loss_v:>3f} [{current:>5d}/{size:>5d}]")
-
-
-def test_loop(network, dataset_loader, loss_func):
-    size = len(dataset_loader.dataset)
-    num_batches = len(dataset_loader)
-    network.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataset_loader:
-            predictions = network(X)
-            test_loss += loss_func(predictions.squeeze(-1), y).item()
-            correct += (predictions.argmax(1) == y).type(torch.float).sum().item()
-    test_loss = test_loss / num_batches
-    correct = correct / size
-    print(
-        f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
-    )
-
-
-def train_model(path_to_data):
-    """
-    Run model training
-    """
-    dataset = Data(path_to_csv=path_to_data)
-    dataset.read_csv()
-    train_dataloader = dataset.get_dataloader(data_type="train")
-    test_dataloader = dataset.get_dataloader(data_type="test")
-    loss_func = nn.BCELoss()
-    network = Network()
-    optimizer = torch.optim.Adam(network.parameters(), lr=LEARNING_RATE)
-
-    for epoch in range(EPOCHS):
-        print(f"Epoch {epoch + 1}\n------")
-        train_loop(network, train_dataloader, loss_func, optimizer)
-        test_loop(network, test_dataloader, loss_func)
+        self.optimizer = Adam(self.network.parameters(), lr=self.learning_rate)
+        for epoch in range(self.epochs):
+            print(f"Epoch {epoch + 1}\n------")
+            self.train_loop(dataset.train_dataloader())
+            self.validation_loop(dataset.test_dataloader())
 
 
 if __name__ == "__main__":
-    train_model("PATH_TO_CSV")
+    modeler = Modeler(
+        loss_function=nn.BCELoss(),
+        epochs=3,
+        learning_rate=2e-5,
+        csv="",
+    )
+    modeler.fit()
